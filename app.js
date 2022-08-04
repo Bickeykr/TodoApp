e2820050acbe706ac3fcc1c02a51a4ab9cf55013
Bickeykr <79741948+Bickeykr@users.noreply.github.com>
2022-08-04 21:30:19 +0530

    changes in app.js

--- app.js
+++ app.js
@@ -155,7 +155,7 @@ let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
 }
-app.listen(port);
+
 
 app.listen(port, function() {
   console.log("Server started successfully");
